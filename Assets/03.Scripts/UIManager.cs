984f595829d828333c0eb5b9028169219902575c
SleepyBear <49630123+whdgns9877@users.noreply.github.com>
2022-09-27 16:53:55 +0900

    Bomb my work But Recover ^^
    Bomb my work But Recover ^^

--- Assets/03.Scripts/UIManager.cs
+++ Assets/03.Scripts/UIManager.cs
@@ -13,29 +13,35 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class UIManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] TextMeshProUGUI Text_ConnectionInfo = null;  // 현재 네트워크 상태 메세지를 나타낼 TextMeshPro
+    [SerializeField] GameObject      Panel_Notice        = null;  // 현재 알림 상태를 띄울 패널
 
     [Header("** 로그인 UI **")]
     [SerializeField] TMP_InputField InputField_NickName = null;  // 닉네임을 입력받을 InputField
     [SerializeField] Button         Button_JoinLobby    = null;  // 로비 접속 버튼
 
     [Header("** 로비 UI **")]
-    [SerializeField] GameObject     Panel_Login             = null;  // 로그인 패널
-    [SerializeField] GameObject     Panel_Lobby             = null;  // 로비 패널
-    [SerializeField] GameObject     Panel_CreateRoom        = null;  // 방을 생성하는데 쓰일 패널
-    [SerializeField] Button         Button_CreateRoomPanel  = null;  // 방생성 판넬을 띄워줄 버튼
-    [SerializeField] Transform      Tr_Content_Room         = null;  // 방 생성시 스크롤뷰에 넣어줄 방의 위치(Vertical Layout Group 사용으로 알맞게 들어가게 할것임)
-    [SerializeField] Button         Button_CreateRoom       = null;  // 방을 생성하는 버튼
-    [SerializeField] TMP_InputField InputField_RoomName     = null;  // 방 이름을 받을 인풋필드
-    [SerializeField] GameObject     room                    = null;  // 방정보에 따라 만들어줄 방 프리팹
-    [SerializeField] Toggle[]       togglesForMaxPlayer     = null;  // 최대 플레이어를 정해줄 토글들
+    [SerializeField] GameObject      Panel_Login             = null;  // 로그인 패널
+    [SerializeField] GameObject      Panel_Lobby             = null;  // 로비 패널
+    [SerializeField] GameObject      Panel_CreateRoom        = null;  // 방을 생성하는데 쓰일 패널
+    [SerializeField] Button          Button_CreateRoomPanel  = null;  // 방생성 판넬을 띄워줄 버튼
+    [SerializeField] Transform       Tr_Content_Room         = null;  // 방 생성시 스크롤뷰에 넣어줄 방의 위치(Vertical Layout Group 사용으로 알맞게 들어가게 할것임)
+    [SerializeField] Button          Button_CreateRoom       = null;  // 방을 생성하는 버튼
+    [SerializeField] TMP_InputField  InputField_RoomName     = null;  // 방 이름을 받을 인풋필드
+    [SerializeField] GameObject      room                    = null;  // 방정보에 따라 만들어줄 방 프리팹
+    [SerializeField] Toggle[]        togglesForMaxPlayer     = null;  // 최대 플레이어를 정해줄 토글들
+    [SerializeField] TextMeshProUGUI Text_MyCost             = null;  // 나의 코스트
 
     [Header("** 방 UI **")]
-    [SerializeField] TextMeshProUGUI   Text_roomName    = null;    // 방 이름
-    [SerializeField] GameObject        Panel_Room       = null;    // 방의 전체적인 패널
-    //[SerializeField] TextMeshProUGUI[] PlayerNickNames  = null;    // 플레이어들의 닉네임을 담을 배열
-    [SerializeField] GameObject        Button_StartGame = null;    // 게임 시작 버튼
-    [SerializeField] GameObject        Button_Ready     = null;    // 게임 레디 버튼
-    [SerializeField] GameObject[]      Panel_PlayerSlot = null;    // 플레이어들이 들어올수 있는 슬롯
+    [SerializeField] TextMeshProUGUI   Text_roomName           = null;    // 방 이름
+    [SerializeField] TextMeshProUGUI   Text_RoomCost           = null;    // 방 코스트
+    [SerializeField] TextMeshProUGUI   Text_MyCostInRoom       = null;    // 방 안에서의 나의 코스트
+    [SerializeField] GameObject        Panel_Room              = null;    // 방의 전체적인 패널
+    [SerializeField] GameObject        Button_StartGame        = null;    // 게임 시작 버튼
+    [SerializeField] GameObject        Button_Ready            = null;    // 게임 레디 버튼
+    [SerializeField] GameObject[]      Panel_PlayerSlot        = null;    // 플레이어들이 들어올수 있는 슬롯
+
+    // 방 입장과 게임에 쓰일 코스트
+    private int myCost = 0;
 
     // 닉네임에 쓰일 InputField
     private string nickNameInputField = "";
@@ -65,6 +71,8 @@ public class UIManager : MonoBehaviourPunCallbacks
         Screen.SetResolution(960, 540, false);
         // 방장(마스터 클라이언트)가 게임씬으로 이동할때 클라이언트들도 같이 이동
         PhotonNetwork.AutomaticallySyncScene = true;
+        // DAPPX를 위한 Cost
+        myCost = 10000;
     }
 
     // Start is called before the first frame update
@@ -89,8 +97,9 @@ public class UIManager : MonoBehaviourPunCallbacks
     public override void OnDisconnected(DisconnectCause cause)
     {
         Text_ConnectionInfo.text = "마스터 서버에서 끊어짐...";
-        // 접속이 끊어진 상태에서는 로비 패널을 비활성화하고 
-        Panel_Lobby.SetActive(false);
+        // 접속이 끊어진 상태에서는 로비 패널을 비활성화하고
+        if(Panel_Lobby.activeInHierarchy)
+            Panel_Lobby.SetActive(false);
         // 닉네임을 입력받는 InputField와 입장 Button을 활성화 시켜준다
         InputField_NickName.interactable = true;
         Button_JoinLobby.interactable = true;
@@ -140,7 +149,9 @@ public class UIManager : MonoBehaviourPunCallbacks
     // 로비에 연결 완료시 호출되는 함수
     public override void OnJoinedLobby()
     {
+        // 로비에서 표시할 텍스트 들을 띄워줌
         Text_ConnectionInfo.text = "로비 접속 완료!";
+        Text_MyCost.text = "MyCost : " + myCost.ToString();
         // 로비에 접속 완료시 로그인 패널은 비활성화 로비 패널은 활성화 해준다
         Panel_Lobby.SetActive(true);
         InputField_NickName.interactable = false;
@@ -162,7 +173,7 @@ public class UIManager : MonoBehaviourPunCallbacks
     // 룸이 삭제되는 경우
     // 룸의 IsOpen 값이 변화할 경우
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    {
+    {       
         // 방의 정보가 바뀌어 이 콜백함수가 실행되면
         // 원래 있던 방들을 전부 없애주고
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("ROOM"))
@@ -197,6 +208,7 @@ public class UIManager : MonoBehaviourPunCallbacks
             roomData.maxPlayer = roomInfo.MaxPlayers;
             roomData.playerCount = roomInfo.PlayerCount;
             roomData.isOpen = roomInfo.IsOpen;
+            roomData.roomCost = roomInfo.MaxPlayers * 1000;
             roomData.UpdateInfo();
 
             // 해당 방의 인원이 꽉차있으면 버튼클릭을 막아 접속할수 없게한다
@@ -268,6 +280,8 @@ public class UIManager : MonoBehaviourPunCallbacks
     {
         // 현재 방에 달려있는 태그를 Hashtable 형식인 curRoomProperties 라는 변수에 넣어준다
         curRoom = PhotonNetwork.CurrentRoom;
+        // 현재 있는 방의 UI 의 Text에 현재 방의 text를 넣어준다
+        Text_roomName.text = "방 : " + curRoom.Name;
 
         if (PhotonNetwork.IsMasterClient)
         {
@@ -275,12 +289,14 @@ public class UIManager : MonoBehaviourPunCallbacks
             int max = curRoom.MaxPlayers - 1;
             //// 방장이 처음에 방을 파게되면 방의 초기 슬롯들의 설정을 해준다
             curRoom.SetCustomProperties(new Hashtable
-            {                
+            {
                 // 방장(호스트)는 0에 본인 번호, 참여가능 슬롯은0, 참여불가능 슬롯은 -1
                 // 해당 슬롯의 인덱스와 방옵션의 MaxPlayer를 비교하여 MaxPlayer가 넘어가는 슬롯은 닫아준다
                 {"0", PhotonNetwork.LocalPlayer.ActorNumber }, {"1", 0 },
                 {"2", 2 <= max ? 0 : -1 }, {"3", 3 <= max ? 0 : -1 }, {"4", 4 <= max ? 0 : -1 },
-                {"5", 5 <= max ? 0 : -1 }, {"6", 6 <= max ? 0 : -1 }, {"7", 7 <= max ? 0 : -1 } });
+                {"5", 5 <= max ? 0 : -1 }, {"6", 6 <= max ? 0 : -1 }, {"7", 7 <= max ? 0 : -1 },
+                {"RoomCost", curRoom.MaxPlayers*1000}
+            });
         }
         else
         {
@@ -298,7 +314,8 @@ public class UIManager : MonoBehaviourPunCallbacks
         // 방에 참가하면 준비상태를 false로
         SetLocalTag("IsReady", false);
 
-        Text_roomName.text = "방 : " + curRoom.Name;
+        myCost -= 1000;
+        Text_MyCostInRoom.text = "MyCost : " + myCost.ToString();
 
         // 패널들의 UI들을 맞게 처리해준다
         Panel_Login.SetActive(false);
@@ -315,6 +332,11 @@ public class UIManager : MonoBehaviourPunCallbacks
         while (PhotonNetwork.InRoom)
         {
             yield return delayUpdateTime;
+            // 업데이트 도중 방에서 나가게되면 방정보 갱신을 멈춘다
+            if(!PhotonNetwork.InRoom) yield break;
+
+            Text_RoomCost.text = "방 베팅 금액 " + ((int)curRoom.CustomProperties["RoomCost"]).ToString();
+
             // 방장이 바뀌었을때 바뀐플레이어가 방장이면 해당 플레이어의 게임시작버튼이 활성화 된다
             if (PhotonNetwork.IsMasterClient)
             {
@@ -329,67 +351,90 @@ public class UIManager : MonoBehaviourPunCallbacks
             }
 
             // 플레이어들 레디 조건 설정
-            for(int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
-                // 슬롯에 플레이어는 없고 사람번호가 있으면 방장이 0을 대입해준다
+                // 방장(마스터클라이언트)이 방설정을 해준다
                 if (PhotonNetwork.IsMasterClient)
                 {
+                    // 슬롯이 열려있는데 사람은 비어있으면 해당 슬롯에 0을 대입
                     if (GetPlayer(i) == null && GetRoomTag(i) > 0) SetRoomTag(i, 0);
-                    else Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(false);
                 }
 
                 // 방 태그를 줄때 열리고 닫히고를 0, -1로 태그값을 줬으므로 이를 통해 판단
                 if (GetRoomTag(i) == -1)
                 {
+                    // 슬롯이 닫혔다는 UI 이미지를 띄워준다
                     Panel_PlayerSlot[i].transform.GetChild(4).gameObject.SetActive(true);
                 }
                 else if (GetRoomTag(i) > 0)
                 {
-                    // 슬롯이 열려있는 상태인데 플레이어가 없다면 비우는 처리
-                    if (GetPlayer(i) == null)
+                    // 반대경우에는 닫힌이미지를 비활성해 열려있는것처럼 보이게 처리
+                    Panel_PlayerSlot[i].transform.GetChild(4).gameObject.SetActive(false);
+                }
+
+                // 슬롯이 열려있는 상태인데 플레이어가 없다면 비우는 처리
+                if (GetPlayer(i) == null)
+                {
+                    Panel_PlayerSlot[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
+                    Panel_PlayerSlot[i].transform.GetChild(1).gameObject.SetActive(false);
+                    Panel_PlayerSlot[i].transform.GetChild(2).gameObject.SetActive(false);
+                    Panel_PlayerSlot[i].transform.GetChild(3).gameObject.SetActive(false);
+                    Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(false);
+                }
+                // 반대 경우라면 해당 플레이어 정보 넣어주는 처리
+                else
+                {
+                    Panel_PlayerSlot[i].GetComponentInChildren<TextMeshProUGUI>().text = GetPlayer(i).NickName; ;
+                    Panel_PlayerSlot[i].transform.GetChild(1).gameObject.SetActive(true);
+                    Panel_PlayerSlot[i].transform.GetChild(3).gameObject.SetActive(true);
+
+                    // 방장은 본인의 방장표시를 켜줌
+                    if (GetPlayer(i).IsMasterClient)
                     {
-                        Panel_PlayerSlot[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
-                        Panel_PlayerSlot[i].transform.GetChild(1).gameObject.SetActive(false);
                         Panel_PlayerSlot[i].transform.GetChild(2).gameObject.SetActive(false);
-                        Panel_PlayerSlot[i].transform.GetChild(3).gameObject.SetActive(false);
+                        Panel_PlayerSlot[i].transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "방 장";
+                        Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(true);
                     }
-                    // 반대 경우라면 해당 플레이어 정보 넣어주는 처리
                     else
                     {
-                        Panel_PlayerSlot[i].GetComponentInChildren<TextMeshProUGUI>().text = GetPlayer(i).NickName; ;
-                        Panel_PlayerSlot[i].transform.GetChild(1).gameObject.SetActive(true);
-                        Panel_PlayerSlot[i].transform.GetChild(3).gameObject.SetActive(true);
-
-                        // 방장은 본인의 방장표시를 켜줌
-                        if (GetPlayer(i).IsMasterClient)
-                        {
-                            Panel_PlayerSlot[i].transform.GetChild(2).gameObject.SetActive(false);
-                            Panel_PlayerSlot[i].transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "방 장";
-                            Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(true);
-                        }
-                        else
-                        {
-                            // 클라이언트들은 본인의 레디상태에 따라 레디이미지를 띄우거나 내리고 방장표시는 내린다
-                            Panel_PlayerSlot[i].transform.GetChild(2).gameObject.SetActive((bool)GetPlayer(i).CustomProperties["IsReady"]);
-                            Panel_PlayerSlot[i].transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "READY";
-                            Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(false);
-                        }
+                        // 클라이언트들은 본인의 레디상태에 따라 레디이미지를 띄우거나 내리고 방장표시는 내린다
+                        Panel_PlayerSlot[i].transform.GetChild(2).gameObject.SetActive((bool)GetPlayer(i).CustomProperties["IsReady"]);
+                        Panel_PlayerSlot[i].transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "READY";
+                        Panel_PlayerSlot[i].transform.GetChild(5).gameObject.SetActive(false);
                     }
                 }
             }
         }
     }
 
-    public void OnLeaveRoomButtonClicked() => PhotonNetwork.LeaveRoom();
-    
+    public void OnLeaveRoomButtonClicked()
+    {
+        PhotonNetwork.LeaveRoom();
+        myCost += 1000;
+        Text_MyCost.text = myCost.ToString();
+    }
 
     // 플레이어가 방을 나갔을때 해당 콜백함수가 실행
     public override void OnLeftRoom()
     {
+        ResetMyRoom();
         // UI들을 상황에 맞게 처리
         Panel_Room.SetActive(false);
         Panel_Login.SetActive(true);
         Panel_Lobby.SetActive(true);
+    }
+
+    private void ResetMyRoom()
+    {
+        // 방을 나가면 다음방에 들어가기전 UI처리들을 초기화하여준다
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Panel_PlayerSlot[i].transform.GetChild(j).gameObject.SetActive(false);
+                Panel_PlayerSlot[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+            }
+        }
     }
 
     private void Update()
@@ -421,37 +466,27 @@ public class UIManager : MonoBehaviourPunCallbacks
             InputField_RoomName.text = null;
     }
 
-    public void SetRoomTag(int slotIndex, int value)
-    {
-        curRoom.SetCustomProperties(new Hashtable { { slotIndex.ToString(), value } });
-    }
+    private void SetRoomTag(int slotIndex, int value) => curRoom.SetCustomProperties(new Hashtable { { slotIndex.ToString(), value } });
 
-    public int GetRoomTag(int slotIndex)
-    {
-        if (curRoom == null) return -2;
-        return (int)curRoom.CustomProperties[slotIndex.ToString()];
-    }
 
-    Player GetPlayer(int slotIndex)
+    private int GetRoomTag(int slotIndex) => (int)curRoom.CustomProperties[slotIndex.ToString()];
+    
+
+    private Player GetPlayer(int slotIndex)
     {
         for(int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
-            int getRoomTag = GetRoomTag(slotIndex);
-            if (PhotonNetwork.PlayerList[i].ActorNumber == getRoomTag)
+            if (PhotonNetwork.PlayerList[i].ActorNumber == GetRoomTag(slotIndex))
                 return PhotonNetwork.PlayerList[i];
         }
         return null;
     }
 
-    public void SetLocalTag(string key, bool value)
-    {
-        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { key, value } });
-    }
+    private void SetLocalTag(string key, bool value) => PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { key, value } });
 
-    public object GetLocalTag(string key)
-    {
-        return (bool)PhotonNetwork.LocalPlayer.CustomProperties[key];
-    }
+
+    //private object GetLocalTag(string key) => (bool)PhotonNetwork.LocalPlayer.CustomProperties[key];
+    
 
     public void OnClick_ReadyButton()
     {
@@ -471,7 +506,10 @@ public class UIManager : MonoBehaviourPunCallbacks
             PhotonNetwork.LoadLevel("GameScene");
         }
         else
-            Debug.Log("아직 모든 플레이어가 레디상태가 아닙니다");
+        {
+            Panel_Notice.GetComponentInChildren<TextMeshProUGUI>().text = "아직 모든 플레이어가 준비 되지 않았습니다";
+            Panel_Notice.SetActive(true);
+        }
     }
 
     public bool CheckPlayersReady()
@@ -483,7 +521,7 @@ public class UIManager : MonoBehaviourPunCallbacks
                 readyCnt++;
         }
 
-        if (readyCnt == curRoom.PlayerCount - 1)
+        if (readyCnt == curRoom.MaxPlayers - 1)
             return true;
         else
             return false;
